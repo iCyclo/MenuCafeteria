f79f65a38b03831520c8eefa835e91f2b891dcce
Daniel Perez <60562714+iCyclo@users.noreply.github.com>
2024-06-13 04:04:23 -0600

    [fixed]: merge conflicts

--- backend/src/main/java/com/example/backend/repository/CategoriaRepository.java
+++ backend/src/main/java/com/example/backend/repository/CategoriaRepository.java
@@ -4,4 +4,5 @@ import com.example.backend.entity.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+    Categoria findByNombre(String nombre);
 }
